7e5037d83a0b33a98b685bea1bfe611fa6ffab99
Daxito <github.ether823@passmail.net>
2025-03-11 20:17:15 +0100

    add get, create, and delete views for store entity

--- src/app/components/stores/form-store/form-store.component.ts
+++ src/app/components/stores/form-store/form-store.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Store, StoreService } from '@aldanetech/accounting-api-client-angular';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-form-store',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class FormStoreComponent {
 
+  display: boolean = false;
+  name: string = '';
+  comment: string = '';
+  @Output() storeCreated = new EventEmitter<string>();
+  @Input() store?: Store;
+  
+  constructor(private storeService: StoreService){}
+  
+  closeDialog() {
+    this.display = false;
+  }
+
+  addStore() {
+    this.display = true;
+  }
+
+  submitForm() {
+    let store: Store = {
+      status: { id: 1},
+      name: this.name,
+      comment: this.comment
+    };
+
+    this.storeService.createStore(store).subscribe({
+      next: (object) => {
+        this.storeCreated.emit();
+        this.closeDialog();
+        this.name = '';
+        this.comment = '';
+      },
+      error: (msg) => {
+
+      }
+    })
+  }
 }
--- src/app/components/stores/stores.component.ts
+++ src/app/components/stores/stores.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Store, StoreService } from '@aldanetech/accounting-api-client-angular';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-stores',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class StoresComponent {
 
+  stores: Store[] = [];
+  
+  constructor(private storeService: StoreService, private cdr: ChangeDetectorRef){
+    this.storeService.getStores().subscribe((stores) => {
+      this.stores = stores;
+    });
+  }
+
+  updateStore(store: Store){
+
+  }
+
+  deleteStore(store: Store){
+    let storeId = store?.id;
+    if(storeId !== undefined){
+      this.storeService.deleteStore(storeId).subscribe(() => {
+        this.stores = this.stores.filter((c) => c.id !== store.id);
+      });
+    }
+  }
+
+  storeCreated(data: any){
+    this.storeService.getStores().subscribe((stores) => {
+      this.stores = stores;
+    });
+  }
 }
