c1bde8f11e542318c89c41ede8d0054fc7ea72f4
Daxito <github.ether823@passmail.net>
2025-03-18 15:31:00 +0100

    crud brand component

--- src/app/components/brands/form-brand/form-brand.component.spec.ts
+++ src/app/components/brands/form-brand/form-brand.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormBrandComponent } from './form-brand.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { TableModule } from 'primeng/table';
 
 describe('FormBrandComponent', () => {
   let component: FormBrandComponent;
@@ -8,7 +13,8 @@ describe('FormBrandComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [FormBrandComponent]
+      declarations: [FormBrandComponent],
+      imports: [HttpClientTestingModule, TableModule, DialogModule, ButtonModule, FormsModule]
     })
     .compileComponents();
     
